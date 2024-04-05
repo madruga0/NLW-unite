725755eccc47ed53e6d18e6dd0506b5f75a4c271
Pedro Madruga <pedromadruga6@gmail.com>
2024-04-04 22:57:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,160 @@
+let participantes = [
+  {
+    nome: "Diego Fernandes",
+    email: "diego@gmail.com",
+    dataInscricao: new Date(2024, 2, 1, 19, 23),
+    dataCheckIn: null
+  },
+  {
+    nome: "Mayk Brito",
+    email: "mayk@gmail.com",
+    dataInscricao: new Date(2024, 2, 23, 19, 23),
+    dataCheckIn: new Date(2024, 2, 25, 20, 20)
+  },
+  {
+    nome: "Ana Souza",
+    email: "ana@gmail.com",
+    dataInscricao: new Date(2024, 0, 3, 19, 23),
+    dataCheckIn: new Date(2024, 0, 4, 20, 20)
+  },
+  {
+    nome: "João Silva",
+    email: "joao@gmail.com",
+    dataInscricao: new Date(2023, 11, 4, 19, 23),
+    dataCheckIn: new Date(2023, 11, 5, 20, 20)
+  },
+  {
+    nome: "Maria Oliveira",
+    email: "maria@gmail.com",
+    dataInscricao: new Date(2023, 10, 5, 19, 23),
+    dataCheckIn: null
+  },
+  {
+    nome: "Pedro Santos",
+    email: "pedro@gmail.com",
+    dataInscricao: new Date(2023, 9, 6, 19, 23),
+    dataCheckIn: new Date(2023, 9, 7, 20, 20)
+  },
+  {
+    nome: "Carlos Costa",
+    email: "carlos@gmail.com",
+    dataInscricao: new Date(2023, 8, 7, 19, 23),
+    dataCheckIn: null
+  },
+  {
+    nome: "Lucas Sousa",
+    email: "lucas@gmail.com",
+    dataInscricao: new Date(2023, 7, 8, 19, 23),
+    dataCheckIn: new Date(2023, 7, 9, 20, 20)
+  },
+  {
+    nome: "Paula Coimbra",
+    email: "paula@gmail.com",
+    dataInscricao: new Date(2023, 6, 9, 19, 23),
+    dataCheckIn: new Date(2023, 6, 10, 20, 20)
+  },
+  {
+    nome: "Gabriel Almeida",
+    email: "gabriel@gmail.com",
+    dataInscricao: new Date(2023, 5, 10, 19, 23),
+    dataCheckIn: new Date(2023, 5, 11, 20, 20)
+  }
+];
+
+
+const criarNovoParticipante = (participante) => {
+  const dataInscricao = dayjs(Date.now()).to(participante.dataInscricao)
+
+  let dataCheckIn = dayjs(Date.now()).to(participante.dataCheckIn)
+
+  
+  if(participante.dataCheckIn == null) {
+    
+    dataCheckIn = `
+    <button
+        data-email="${participante.email}"
+        onclick="fazerCheckIn(event)"
+    >
+      	Confirmar check-in
+    </button>
+    `
+  }
+
+  return `
+  <tr>
+  <td>
+    <strong>
+      ${participante.nome}
+    </strong>
+      <br>
+      <small>
+      ${participante.email}
+      </small>
+    </td>
+  <td>${dataInscricao}</td>
+  <td>${dataCheckIn}</td>
+</tr>
+`
+}
+
+const atualizarLista = (participantes) => {
+  // estrutura de repeticao = loop
+  let output = ""
+  for(let participante of participantes) {
+   output = output + criarNovoParticipante(participante)
+  }
+  // substituir informacao do HTML
+  document
+    .querySelector('tbody')
+    .innerHTML = output
+}
+
+atualizarLista(participantes)
+
+const adicionarParticipante = (event) => {
+  event.preventDefault()
+
+  const dadosDoFormulario = new FormData(event.target)
+
+  const participante = {
+    nome: dadosDoFormulario.get('nome'),
+    email: dadosDoFormulario.get('email'),
+    dataInscricao: new Date(),
+    dataCheckIn: null
+  }
+
+
+  const participanteExiste = participantes.find(
+    // forma de encurtar a arrow function
+    (p) => p.email == participante.email
+    )
+
+  if(participanteExiste) {
+    alert('Email já cadastrado!')
+    return
+  }
+
+participantes = [participante, ...participantes]
+atualizarLista(participantes)
+
+
+event.target.querySelector('[name="nome"]').value = ""
+event.target.querySelector('[name="email"]').value = ""
+}
+
+const fazerCheckIn = (event) => {
+
+  const mensagemConfirmacao = 'Tem certeza de que deseja fazer o check-in?'
+  if(confirm(mensagemConfirmacao) == false) {
+    return 
+  }
+
+  const participante =
+  // forma de encurtar a arrow function
+   participantes.find((p) => p.email == event.target.dataset.email
+  )
+
+  participante.dataCheckIn = new Date()
+
+  atualizarLista(participantes)
+}
---
+++ index.md
@@ -0,0 +1,51 @@
+table = criar tabela
+
+
+
+
+```css 
+/* declarações */
+body {
+  background-color: #121214;
+  color: #ffffff;
+}
+
+/* variável no css 
+root: --propriedade: */
+:root {
+  --border: 1px solid #ffffff1a;
+}
+
+/* para evitar o paddin e marign da pagina*/
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+```
+
+
+``` js text
+// objeto js
+const participante = {
+  nome: "Livia Madruga",
+  email: "liviamadrugaa@gmail.com",
+  dataInscricao: new Date(2024, 2, 22, 19, 20),
+  dataCheckIn: new Date(2024, 2, 25, 22, 00),
+}
+
+let participantes = [
+  {
+    nome: "Livia Madruga",
+    email: "liviamadrugaa@gmail.com",
+    dataInscricao: new Date(2024, 2, 22, 19, 20),
+    dataCheckIn: new Date(2024, 2, 25, 22, 00),
+  },
+]
+// estrutura de repetição - loop
+ for(let participante of participantes) {
+  // faça alguma coisa aqui
+  // enquanto tiver participantes nessa lista
+  }
+```
+
